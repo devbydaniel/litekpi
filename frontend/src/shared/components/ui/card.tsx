aa69fcad70449ce327e2fff877e3a926975584af
Daniel Benner <daniel@devbydaniel.com>
2025-12-23 10:22:08 +0100

    fix: fix card slot

--- frontend/src/shared/components/ui/card.tsx
+++ frontend/src/shared/components/ui/card.tsx
@@ -24,7 +24,7 @@ const CardHeader = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 p-6 has-[data-slot=card-action]:grid-cols-[1fr_auto]',
+      '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 p-6 has-[[data-slot="card-action"]]:grid-cols-[1fr_auto]',
       className
     )}
     {...props}
